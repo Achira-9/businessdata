0eb9db0fe2f86a1563ae4960ac865dfa3b013f96
Achira-9 <6631501120@lamduan.mfu.ac.th>
2026-04-18 12:22:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predictions, preprocess_input
 from tensorflow.keras.preprocessing import image
@@ -50,48 +50,4 @@ if uploaded_file is not None:
 
         # show class
         st.write(f"**{i+1}. {class_name}** — {confidence:.2f}%")
-        st.progress(int(confidence))
-
-st.set_page_config(page_title="Image Classifier", page_icon="📸")
-st.title("📸 AI Image Classifier (MobileNetV2)")
-st.write("Upload an image (.jpg, .jpeg, .png) and the AI will predict what it is!")
-
-
-@st.cache_resource
-def load_model():
-    return MobileNetV2(weights="imagenet")
-    model = load_model()
-
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    # open and upload file
-    img = Image.open(uploaded_file)
-    # Updated 'use_container_width' to the new 2026 standard 'width="stretch"'
-    st.image(img, caption='Uploaded Image', width="stretch")
-
-    st.write("🔄 Classifying...")
-
-
-    img_resized = img.resize((224, 224))
-    x = image.img_to_array(img_resized)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-
-
-
-    preds = model.predict(x)
-
-
-    top_preds = decode_predictions(preds, top=3)[0]
-
-
-    st.subheader("Predictions:")
-    for i, pred in enumerate(top_preds):
-        classname = pred[1].replace('', ' ').title() # setting the font format
-        confidence = pred[2] * 100
-
-        # show class
-        st.write(f"{i+1}. {class_name} — {confidence:.2f}%")
         st.progress(int(confidence))
